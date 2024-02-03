e2c8553623143a96f78249498b674325f2cdd6cd
Serhii Borozennyi <Serhii@thecareeros.com>
2024-02-03 17:34:37 +0100

    v 0.2.12

--- server/src/index.js
+++ server/src/index.js
@@ -18,6 +18,10 @@ const httpServer = createServer(app)
 //? listener will be on httpServer
 const io = new Server(httpServer, {
   //
+  сors: {
+    origin: "https://multiplayer-game-production-7342.up.railway.app",
+    methods: ["GET", "POST"],
+  },
 });
 
 io.on('connect', (socket) => {
